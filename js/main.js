8418126a202432e4d33945f7f95b8acb9b6ba892
samandar-dev <samandarbohodirov01@gmail.com>
2022-01-21 19:59:50 +0500

    creat

---
+++ js/main.js
@@ -0,0 +1,57 @@
+let elopenmodal = document.getElementById('openModal');
+let elColsemodal = document.getElementById('close-btn');
+let elModal = document.getElementById('Modal');
+
+
+elopenmodal.addEventListener('click', function () {
+  elModal.classList.add('show');
+});
+
+elColsemodal.addEventListener('click', function () {
+  elModal.classList.remove('show');
+});
+
+
+let elopennmodal = document.getElementById('opennModal');
+let elColseemodal = document.getElementById('closee-btn');
+let elbagModal = document.getElementById('bagModal');
+
+
+elopennmodal.addEventListener('click', function () {
+  elbagModal.classList.add('bagModal');
+});
+
+elColseemodal.addEventListener('click', function () {
+  elbagModal.classList.remove('bagModal');
+});
+
+
+let elopenShopnmodal = document.querySelector('.shop__img-icon');
+let elColseEemodal = document.getElementById('closeEe-btn');
+let elbagShopModal = document.getElementById('mod-show-bag-shop');
+
+
+elopenShopnmodal.addEventListener('click', function () {
+  elbagShopModal.classList.add('mod-show-bag-shop');
+});
+
+
+elColseEemodal.addEventListener('click', function () {
+  elbagShopModal.classList.remove('mod-show-bag-shop');
+});
+
+
+
+let elopenHeartnmodal = document.getElementById('open-heart_show');
+let elheartColseEemodal = document.getElementById('heart-close-btn');
+let elbagHeartModal = document.getElementById('heart_show');
+
+
+elopenHeartnmodal.addEventListener('click', function () {
+  elbagHeartModal.classList.add('heart_show');
+});
+
+
+elheartColseEemodal.addEventListener('click', function () {
+  elbagHeartModal.classList.remove('heart_show');
+});
